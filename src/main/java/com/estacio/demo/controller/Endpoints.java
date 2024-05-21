70933b9e025de6d6e777a753f8ff177205696114
Henry B Silva <hb.personal.ctt@gmail.com>
2024-05-20 23:09:34 -0300

    Cors config

--- src/main/java/com/estacio/demo/controller/Endpoints.java
+++ src/main/java/com/estacio/demo/controller/Endpoints.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * HelloController
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", methods = RequestMethod.GET)
+@CrossOrigin(origins = "http://localhost:3000")
 public class Endpoints {
     String getJSONValue (String key, String jsonObject) {
         JSONObject object = new JSONObject(jsonObject);
